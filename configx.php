1fdf5797027f494c244f15c46659911fce6e0d35
root <yeganx>
2017-01-14 14:17:02 +0330

    mysql host name corrected

--- configx.php
+++ configx.php
@@ -1,8 +1,14 @@
 <?php
-$url = "http://locolhost/reg";
+/*$url = "http://locolhost/reg";
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db = "test";
+$db = "test";*/
 /*$connecDB = mysqli_connect($host, $user,$pass,$db)or die('could not connect to database');*/
+
+$host = getenv("MYSQL_SERVICE_HOST");
+$dbport = getenv("MYSQL_SERVICE_PORT");
+$user = getenv("MYSQL_USER");
+$pass = getenv("MYSQL_PASSWORD");
+$db = getenv("MYSQL_DATABASE");
 ?>
